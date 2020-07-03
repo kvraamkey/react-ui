5829c7b86bc5faba1a290687788a19d5a50f3aac
ramki <ramki.ayyamperumal@everestdx.com>
2020-07-04 00:33:15 +0530

    [FEAT] radio button added

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,46 +1,37 @@
-import * as React from 'react';
-import classNames from 'classnames';
+import * as React from "react";
+import classNames from "classnames";
 
 type ButtonProps = {
-  name: string,
-  as?: string,
-  variant?: string,
-  href?: string,
-  link: boolean,
-  className?: 'string'
+    name: string,
+    as?: string,
+    variant?: string,
+    href?: string,
+    link: boolean,
+    className?: "string"
 };
 
 const Button = (props: ButtonProps) => {
-  const {
-    as: Element,
-    variant: buttonType,
-    link,
-    outline,
-    className,
-    disabled
-  } = props;
-  var btnClass = classNames('c-btn', {
-    [`c-btn__${buttonType}`]: buttonType ? true : false,
-    'c-btn__outline': outline ? true : false,
-    'c-btn__link': link ? true : false
-  });
+    const { as: Element, variant: buttonType, link, outline, className, disabled } = props;
+    var btnClass = classNames("c-btn", {
+        [`c-btn__${buttonType}`]: buttonType ? true : false,
+        "c-btn__outline": outline ? true : false,
+        "c-btn__link": link ? true : false
+    });
 
-  return (
-    <Element className={classNames(btnClass, className)} disabled={disabled}>
-      {props.name}
-    </Element>
-  );
+    return (
+        <Element className={classNames(btnClass, className)} disabled={disabled}>
+            {props.name}
+        </Element>
+    );
 };
 
-const ButtonWrapper = (props) => (
-  <div className='c-btn__list'>{props.children}</div>
-);
+const ButtonWrapper = (props) => <div className='c-btn__list'>{props.children}</div>;
 
 Button.List = ButtonWrapper;
 
 Button.defaultProps = {
-  as: 'button',
-  href: '#'
+    as: "button",
+    href: "#"
 };
 
 export default Button;
